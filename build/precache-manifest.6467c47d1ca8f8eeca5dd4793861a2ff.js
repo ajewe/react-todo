1a99744b2cc749c082be89b17ffb2f5e692cab41
ajewe <ajewe@utexas.edu>
2020-03-30 16:53:54 -0500

    build

---
+++ build/precache-manifest.6467c47d1ca8f8eeca5dd4793861a2ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c00adb64c6fb545d8cc4ad7be513b18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b329a4a8db397a814a3",
+    "url": "/static/css/main.a11e4d6b.chunk.css"
+  },
+  {
+    "revision": "9bcebff66a16895891ef",
+    "url": "/static/js/2.c5485d7a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c5485d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b329a4a8db397a814a3",
+    "url": "/static/js/main.8cff1f3f.chunk.js"
+  },
+  {
+    "revision": "6d0f439a5b9c5f5bdea5",
+    "url": "/static/js/runtime-main.aa84ab0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
